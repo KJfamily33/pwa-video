edd183ab23caf2dc88e129568774e2835d7e0cf7
hewerrr <hewerrr@gmail.com>
2018-07-17 16:32:23 +0300

    Initial video with autoplay

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+        var elem = document.getElementById("myvideo");
+        
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.mozRequestFullScreen) {
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) {
+            elem.webkitRequestFullscreen();
+        }
+})();
